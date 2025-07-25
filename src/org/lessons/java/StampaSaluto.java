bc366a38277c8f7b4f2c28c0bd3a402bfef6c587
bradyshehu <shehu.brady@gmail.com>
2025-07-25 13:00:47 +0200

    fix on names

--- src/org/lessons/java/StampaSaluto.java
+++ src/org/lessons/java/StampaSaluto.java
@@ -1,6 +1,6 @@
 package org.lessons.java;
 
-public class SalutoClasse {
+public class StampaSaluto {
  public static void main(String[] args) {
     System.out.println("Ciao specializzazione Java!");
  }
